706006c6dcd6b10b85ccc1f915867ce9ee8e1fc3
Yassine <yassinelutumba.98@gmail.com>
2022-08-08 00:14:07 +0100

    add generic views and update admin site

--- polls/admin.py
+++ polls/admin.py
@@ -1,5 +1,18 @@
 from django.contrib import admin
 from .models import Question, Choice
 
-admin.site.register([Question,Choice])
+class ChoiceInLine(admin.TabularInline):
+    model = Choice
+    extra = 2
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ('Title', {'fields': ['question_text']}),
+        ('Date info', {'fields': ['pub_date']}),
+    ]
+    inlines = [ChoiceInLine]
+    list_display = ('question_text', 'pub_date')
+    #fields = ['pub_date', 'question_text']
+
+admin.site.register(Question, QuestionAdmin)
 # Register your models here.
